18df3f43bfcfa67241008908c6cdec6b1069561f
Rico <r.jambor@yahoo.de>
2017-10-12 15:40:06 +0200

    FEATURE: Add id and method to update price

--- wishlist.py
+++ wishlist.py
@@ -1,9 +1,13 @@
 class Wishlist(object):
 
-    def __init__(self, name, url, price):
+    def __init__(self, id, name, url, price):
+        self.__id = int(id)
         self.__name = str(name)
         self.__url = str(url)
         self.__price = str(price)
+
+    def id(self):
+        return self.__id
 
     def name(self):
         return self.__name
@@ -13,3 +17,6 @@ class Wishlist(object):
 
     def price(self):
         return self.__price
+
+    def update_price(self, new_price):
+        self.__price = new_price
