6f1301b5093bc57dee0f0f8b2e97829a01738dee
Rico <r.jambor@yahoo.de>
2018-03-08 14:35:38 +0100

    TASK: Use @property decorator

--- wishlist.py
+++ wishlist.py
@@ -5,17 +5,34 @@ class Wishlist(object):
         self.__url = str(url)
         self.__price = float(price)
 
+    @property
     def id(self):
         return self.__id
 
+    @id.setter
+    def id(self, new_id):
+        self.__id = new_id
+
+    @property
     def name(self):
         return self.__name
 
+    @name.setter
+    def name(self, new_name):
+        self.__name = new_name
+
+    @property
     def url(self):
         return self.__url
 
+    @url.setter
+    def url(self, new_url):
+        self.__url = new_url
+
+    @property
     def price(self):
         return self.__price
 
-    def update_price(self, new_price):
+    @price.setter
+    def price(self, new_price):
         self.__price = new_price
