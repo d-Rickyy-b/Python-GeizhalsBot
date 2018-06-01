c3e182d5409e97ba97c1432f19cf4ba09e436ca6
Rico <r.jambor@yahoo.de>
2018-06-02 00:51:57 +0200

    TASK: Implement some tests

--- geizhals/tests/core_test.py
+++ geizhals/tests/core_test.py
@@ -1,13 +1,24 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import geizhals.core
+
 
 class Test(unittest.TestCase):
 
     def setUp(self):
-        pass
+        with open("test.html", "r") as f:
+            self.html = f.read()
 
     def tearDown(self):
         pass
+
+    def test_parse_wishlist_price(self):
+        price = geizhals.core.parse_wishlist_price(self.html)
+        self.assertEqual(price, "717.81")
+
+    def test_parse_wishlist_name(self):
+        name = geizhals.core.parse_wishlist_name(self.html)
+        self.assertEqual(name, "NAS")
 
 
