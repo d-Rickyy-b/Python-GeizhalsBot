b263afae1a7c8bbc5632cc1e1a09ad81a8dd03b8
Rico <r.jambor@yahoo.de>
2018-06-15 19:15:40 +0200

    TASK: Add test for send_request

--- geizhals/tests/core_test.py
+++ geizhals/tests/core_test.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import re
 import unittest
 
 import geizhals.core
@@ -16,6 +17,18 @@ class GeizhalsCoreTest(unittest.TestCase):
 
     def tearDown(self):
         pass
+
+    def test_send_request(self):
+        """Test to check if downloading the html code of a website works"""
+        regex = re.compile(r'\s')
+        html = geizhals.core.send_request("http://example.com")
+        example_path = os.path.join(self.dir_path, "example.html")
+
+        with open(example_path, "r") as f:
+            example_html = f.read()
+
+        # Replace all the whitespace to not have any issue with too many or little of them.
+        self.assertEqual(regex.sub("", html), regex.sub("", example_html))
 
     def test_parse_wishlist_price(self):
         price = geizhals.core.parse_wishlist_price(self.html)
