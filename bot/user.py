d4cc041fe9b0b941fba92eb41bf0c2d72cb4a9cd
Rico <r.jambor@yahoo.de>
2018-06-25 22:36:10 +0200

    TASK: Add more type hints

--- bot/user.py
+++ bot/user.py
@@ -6,7 +6,7 @@
 
 class User(object):
 
-    def __init__(self, id: int, first_name: str, username: str, lang_code):
+    def __init__(self, id: int, first_name: str, username: str, lang_code: str):
         self.id = id
         self.first_name = first_name
         self.username = username
