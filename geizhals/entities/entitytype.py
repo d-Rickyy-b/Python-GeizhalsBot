e02b45c04f585cff75f01b4250366ef778191668
Rico <d-Rickyy-b@users.noreply.github.com>
2020-01-06 12:57:15 +0100

    FEATURE: New method to get the name of entity type

--- geizhals/entities/entitytype.py
+++ geizhals/entities/entitytype.py
@@ -5,3 +5,10 @@ from enum import Enum
 class EntityType(Enum):
     WISHLIST = 1
     PRODUCT = 2
+
+    @staticmethod
+    def get_type_article_name(entity_type):
+        if entity_type == EntityType.WISHLIST:
+            return dict(article="die", name="Wunschliste")
+        elif entity_type == EntityType.PRODUCT:
+            return dict(article="das", name="Produkt")
